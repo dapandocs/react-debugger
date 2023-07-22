f0e90504b42d463e76bf0bf5844a55af030e3dbe
react-spy <996251389@qq.com>
2023-07-22 15:47:05 +0800

    🦄 refactor: update eslintrc.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <div>Hello world!</div>;
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(element);
